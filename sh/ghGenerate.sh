b2e54e158921c60098124d6cd0b1395233babc7b
AngeCI <145793112+AngeCI@users.noreply.github.com>
2025-02-13 01:09:19 +0800

    Updated mdBook versions.

---
+++ sh/ghGenerate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+COMPRESS_CRIT="\.(bin|bm|bmp|conf|css|csv|htm|html|ico|js|json|kar|list|lst|map|md|mid|mjs|mod|otf|svg|tsv|ttf|vgm|wasm|webmanifest|xml)$"
+rm -v 'book/FontAwesome/fonts/fontawesome-webfont.eot' 'book/FontAwesome/fonts/fontawesome-webfont.ttf'
+echo "$(date +"%s")" > build-time.txt
+cp -r book book-gz
+cp -r book book-br
+cd book
+tar cvf ../pages-build.tar *
+cd ..
+zopfli --i1 -v pages-build.tar
+rm -v pages-build.tar
+cd book-gz
+tree -ifl | while IFS= read -r file; do
+	if [ -f "$file" ]; then
+		# Is a file
+		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
+			zopfli --i1 "$file" && echo "Compressed \"${file}\" with Zopfli."
+		else
+			echo "File \"${file}\" cannot be compressed."
+		fi
+		if [ -f "$file" ]; then
+			rm -v "$file"
+		fi
+	fi
+done
+tar cvf ../pages-build-gz.tar *
+cd ..
+cd book-br
+tree -ifl | while IFS= read -r file; do
+	if [ -f "$file" ]; then
+		# Is a file
+		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
+			brotli -vq 11 "$file"
+		else
+			echo "File \"${file}\" cannot be compressed."
+		fi
+		if [ -f "$file" ]; then
+			rm -v "$file"
+		fi
+	fi
+done
+tar cvf ../pages-build-br.tar *
+cd ..
+exit
